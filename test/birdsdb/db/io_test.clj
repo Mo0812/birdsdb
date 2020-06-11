1613e23fc227279732d67225e8b1d6c9e0adf98a
Moritz Kanzler <Mo0812@users.noreply.github.com>
2020-06-11 14:54:32 +0200

    added test case for io ns

---
+++ test/birdsdb/db/io_test.clj
@@ -0,0 +1,10 @@
+(ns birdsdb.db.io-test
+  (:require [clojure.test :refer :all]
+            [clojure.test.check.properties :as prop]
+            [clojure.test.check.clojure-test :refer [defspec]]
+            [clojure.spec.alpha :as s]
+            [clojure.spec.gen.alpha :as gen]
+            [clojure.spec.test.alpha :as stest]
+            [birdsdb.db.io :refer :all]))
+            
+  (deftest read-file-test)
